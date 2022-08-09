98e56b6704e057251d7c885e065f78ddf3479546
Baz Murphy <bazmurphy@gmail.com>
2022-08-09 19:23:10 +0100

    changed data file

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,76 @@
 import React from 'react';
+import memesData from "../data/memesData.js"
 
 export default function Meme() {
+    
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getMemeImage() {
+        // console.log('clicked')
+        const memesArray = memesData.data.memes;
+        // console.log(memesArray)
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        // console.log(randomNumber)
+        const url = memesArray[randomNumber].url;
+        console.log(url);
+        setMemeImage(url);
+    }
+ 
     return (
         <main>
-            <form>
+            <div className="form">
                 <input type="text" className="form-top-text" placeholder="Top text"/>
                 <input type="text" className="form-bottom-text" placeholder="Bottom text"/>
-                <button className="form-button">Get a new meme image 🖼️</button>
-            </form>
+                <button onClick={getMemeImage} className="form-button">Get a new meme image 🖼️</button>
+            </div>
+            <img className="meme-image" src={memeImage} alt="" />
         </main>
     );
 }
+
+
+// Event Listener Recap:
+
+// JS way :
+// document.querySelector('button').addEventListener("click", function() {})
+
+// HTML way:
+// <button onclick="myFunction()">Click me</button>
+
+// REACT way:
+// <button onClick={function() {}}>Click me</button>
+
+// GOTCHA :
+// DO NOT PUT handleClick() with Parentheses... because it will run as soon as JSX loads that line of code.. <button....>
+// We are passing the function as a VALUE so React will use that function when a click happens
+
+// REACT Documentation on Mouse Events :
+// https://reactjs.org/docs/events.html#mouse-events
+
+// onClick={}
+
+// onMouseOver={}
+
+// function handleClick() {
+//     console.log("I was clicked!")
+// }
+
+// function handleMouseOver() {
+//     console.log("I was hovered over")
+// }
+
+// const getRandomObjectFromData = (dataObject) => {
+//     const totalDataEntries = dataObject.length;
+//     const randomIndex = Math.floor(Math.random() * totalDataEntries);
+//     return dataObject[randomIndex];
+// }
+
+// const getPropertyFromRandomObject = (dataObject, property) => {
+//     return getRandomObjectFromData(dataObject)[property]
+// }
+
+// const getRandomMeme = () => {
+//     setMemeImage(getRandomMeme());
+// }
+
+// console.log(getRandomMeme());
